Added custom start epoch for gradient loss, minor bug fixes

train.py
@@ -147,16 +147,16 @@ def main_worker(gpu, ngpus_per_node, args):
         #Model.load_state_dict(torch.load(args.model_dir,map_location='cuda:'+args.gpu_num))
         Model = torch.nn.DataParallel(Model)
         Model.load_state_dict(torch.load(args.model_dir))
-        encoder_params = Model.module.encoder.parameters()
-        decoder_params = Model.module.decoder.parameters()
+        enc_param = Model.module.encoder.parameters()
+        dec_param = Model.module.decoder.parameters()
         if (args.rank == 0):
             print('=> pretrained model is created')
     #############################################################################################
 
 
     ############################## optimizer and criterion setting ##############################
-    optimizer = torch.optim.AdamW([{'params': encoder_params, 'weight_decay': args.weight_decay, 'lr': args.lr},
-                                   {'params': decoder_params, 'weight_decay': 0, 'lr': args.lr}], eps=args.adam_eps)
+    optimizer = torch.optim.AdamW([{'params': enc_param, 'weight_decay': args.weight_decay, 'lr': args.lr},
+                                   {'params': dec_param, 'weight_decay': 0, 'lr': args.lr}], eps=args.adam_eps)
     ##############################################################################################
     logger = None
 

trainer.py
@@ -222,7 +222,7 @@ def train_net(args,model, optimizer, dataset_loader,val_loader, n_epochs,logger)
             Gradient Loss is only applied after 20 epochs in the case of NYU dataset
             Reduced start of gradient training to epoch 5 (before 20)
             '''
-            grad_epoch = 15 if args.dataset == 'KITTI' else 5
+            grad_epoch = 15 if args.dataset == 'KITTI' else args.gradient_loss_epoch
             if args.use_dense_depth is True:
                 if epoch < grad_epoch:
                     gradient_loss = torch.tensor(0.).cuda()